d3fc69e1eeabb59cccff69445491a4cf2f3dd905
chenkangqin <chenkq@minstone.com.cn>
2019-04-08 20:10:01 +0800

    commit Apple.java

---
+++ src/FactoryPattern/code/bean/Apple.java
@@ -0,0 +1,23 @@
+package FactoryPattern.code.bean;
+
+/**
+ * @author chenkq
+ */
+public class Apple {
+    private String name;
+
+    public Apple(){
+
+    }
+
+    public Apple(String name){
+        this.name=name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
